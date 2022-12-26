7f63ada01ad85c8246d0e2bc32d2975cdcadc681
Chris Ross <tratcher@outlook.com>
2022-12-25 19:57:24 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using var reader = new StreamReader("Day1Input.txt");
+var line = reader.ReadLine();
+int highest = 0, current = 0;
+while (line != null)
+{
+    if (line == string.Empty)
+    {
+        Console.WriteLine(current);
+        if (current > highest) highest = current;
+        current = 0;
+    }
+    else
+    {
+        current += int.Parse(line);
+    }
+    line = reader.ReadLine();
+}
+Console.WriteLine("Highest: " + highest);
+
